e6ea72466232e1114fa5b7ab8294d0069c69ae53
Daria Kolodkina <daria.kolodkina@emlid.com>
2024-11-27 18:23:52 +0100

    config_device: change commands

--- config_device.sh
+++ config_device.sh
@@ -2,14 +2,14 @@
 # Shell script to easily configure a new device
 
 # Change NVRAM (power-up default) settings 
-./mcp2210_conf set 1 -n spi_settings
-./mcp2210_conf set 1 -n chip_settings
+./mcp2210_conf -n -s spi_settings
+./mcp2210_conf -n -s chip_settings
 
 # Change the settings in RAM too to avoid re-plugging the device
-./mcp2210_conf set 1 spi_settings
-./mcp2210_conf set 1 chip_settings
+./mcp2210_conf -s spi_settings
+./mcp2210_conf -s chip_settings
 
 # Changing these is usually not required
-#./mcp2210_conf set 1 -n key_parameters
-#./mcp2210_conf set 1 -n product_name
-#./mcp2210_conf set 1 -n manufacturer_name
+#./mcp2210_conf -s key_parameters
+#./mcp2210_conf -s product_name
+#./mcp2210_conf -s manufacturer_name
